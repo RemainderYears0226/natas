df145984d0f6b3def893a125a3b43c24b3334ab0
RemainderYears0226 <144605230+RemainderYears0226@users.noreply.github.com>
2024-04-19 12:23:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,35 @@
 
 <script>
     window.onload = function() {
-        document.getElementById("dynamicContent").innerHTML = "<!-- 这里是你想要隐藏的代码 -->";
+        document.getElementById("dynamicContent").innerHTML = "    
+    <script>
+        function checkInput1() {
+            const input = document.getElementById('input1');
+            const checkbox = document.getElementById('checkbox1');
+            if (input.value === 'FHSH{Information_Security_Entry_Engineer}') {
+                checkbox.classList.add('checked');
+            } else {
+                checkbox.classList.remove('checked');
+            }
+        }
+        function checkInput2() {
+            const input = document.getElementById('input2');
+            const checkbox = document.getElementById('checkbox2');
+            if (input.value === 'FHSH{encrypt_decode_so_interesting}') {
+                checkbox.classList.add('checked');
+            } else {
+                checkbox.classList.remove('checked');
+            }
+        }
+        function checkInput3() {
+            const input = document.getElementById('input3');
+            const checkbox = document.getElementById('checkbox3');
+            if (input.value === 'FHSH{Ste9an09raphy}') {
+                checkbox.classList.add('checked');
+            } else {
+                checkbox.classList.remove('checked');   
+            }
+        }
+    </script>";
     };
 </script>
