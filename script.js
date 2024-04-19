687a2a0d6ebb6c93b12933717d6c39273735b725
RemainderYears0226 <144605230+RemainderYears0226@users.noreply.github.com>
2024-04-19 12:21:27 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+<div id="dynamicContent"></div>
+
+<script>
+    window.onload = function() {
+        document.getElementById("dynamicContent").innerHTML = "<!-- 这里是你想要隐藏的代码 -->";
+    };
+</script>
