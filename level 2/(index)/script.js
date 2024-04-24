7da3c15de299a31d2464cb072ca93d956cf085a1
RemainderYears0226 <144605230+RemainderYears0226@users.noreply.github.com>
2024-04-24 11:26:58 +0800

    Update script.js

--- level 2/(index)/script.js
+++ level 2/(index)/script.js
@@ -1,10 +1,8 @@
-// 获取并显示 flag 的函数
-function showFlag() {
-    var messageBox = document.querySelector('.message-box');
-    messageBox.innerHTML += '<p>Flag: Flag is here!</p>';
-}
-
-// 获取 cookie 值的函数
+// 第二關的flag-3/3: Y29kZX0=
+document.addEventListener('contextmenu', function(e) {
+    alert('在這裡右鍵不能使用喔!');
+    e.preventDefault();
+}, false);
 function getCookieValue(cookieName) {
     var cookies = document.cookie.split(';');
     for (var i = 0; i < cookies.length; i++) {
@@ -16,17 +14,27 @@ function getCookieValue(cookieName) {
     return "";
 }
 
-// 检查 cookie 值是否为 1，如果是则显示 flag
+function showFlag() {
+    var messageBox = document.querySelector('.message-box');
+    messageBox.innerHTML += '<p>Flag: Flag is here!</p>';
+}
+
 var loginValue = getCookieValue("login");
+
 if (loginValue === "1") {
     showFlag();
 }
 
-// 将设置 cookie 的函数移动到全局作用域，以便在浏览器开发者工具中调用
-window.setLoginCookie = function(value) {
-    document.cookie = "login=" + value;
-    // 在更改 cookie 值后，检查并显示 flag（如果 cookie 值为 1）
-    if (value === "1") {
-        showFlag();
+function getCookieValue(cookieName) {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length + 1); // 修改這一行
+        }
     }
+    return "";
 }
+
+
+setLoginCookie("0");
