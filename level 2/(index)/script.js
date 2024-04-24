64d8f9024271bebc911413b73a956fb76ec8768a
RemainderYears0226 <144605230+RemainderYears0226@users.noreply.github.com>
2024-04-24 09:47:03 +0800

    Update script.js

--- level 2/(index)/script.js
+++ level 2/(index)/script.js
@@ -1,40 +1,38 @@
-// 檢查 cookie 的函數
-function getCookieValue(cookieName) {
-    // 取得所有的 cookie
-    var cookies = document.cookie.split(';');
-    // 逐一檢查 cookie
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i].trim();
-        // 檢查是否為目標 cookie
-        if (cookie.indexOf(cookieName) === 0) {
-            // 取得 cookie 的值
-            return cookie.substring(cookieName.length + 1);
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FHSH2</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body oncontextmenu="return false;">
+    <div class="header">
+        <h1>FHSH2</h1>
+    </div>
+    <img src="img.jpg" alt="Level 2 Image">
+    <div class="message-box">
+        <p>你可以在這裡找到下一關的Flag，但是我們對它做過了一些處理...</p>
+    </div>
+
+    <script>
+        window.onload = function() {
+            var loginCookie = getCookie("login");
+            if (loginCookie === "1") {
+                var flagBox = document.createElement("div");
+                flagBox.innerHTML = "<p>Flag: Your_Flag_Here</p>";
+                document.body.appendChild(flagBox);
+            }
         }
-    }
-    // 如果找不到目標 cookie，返回空值
-    return "";
-}
 
-// 顯示 flag 的函數
-function showFlag() {
-    // 在 message-box 中顯示 flag
-    var messageBox = document.querySelector('.message-box');
-    messageBox.innerHTML += '<p>Flag: Flag is here!</p>';
-}
-
-// 檢查 login cookie 的值
-var loginValue = getCookieValue("login");
-
-// 如果 login cookie 的值為 1，顯示 flag
-if (loginValue === "1") {
-    showFlag();
-}
-
-// 設置 login cookie 的函數
-function setLoginCookie(value) {
-    // 設置 login cookie 的值
-    document.cookie = "login=" + value;
-}
-
-// 設置 login cookie 的值為 1
-setLoginCookie("1");
+        function getCookie(name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            }
+            return null;
+        }
+    </script>
+</body>
+</html>
