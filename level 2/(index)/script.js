87f1c203096f5220ba4402839a8cb6a963192bf8
RemainderYears0226 <144605230+RemainderYears0226@users.noreply.github.com>
2024-04-24 09:39:26 +0800

    Update script.js

--- level 2/(index)/script.js
+++ level 2/(index)/script.js
@@ -1,5 +1,40 @@
-// 第二關的flag-3/3: Y29kZX0=
-document.addEventListener('contextmenu', function(e) {
-    alert('在這裡右鍵不能使用喔!');
-    e.preventDefault();
-}, false);
+// 檢查 cookie 的函數
+function getCookieValue(cookieName) {
+    // 取得所有的 cookie
+    var cookies = document.cookie.split(';');
+    // 逐一檢查 cookie
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        // 檢查是否為目標 cookie
+        if (cookie.indexOf(cookieName) === 0) {
+            // 取得 cookie 的值
+            return cookie.substring(cookieName.length + 1);
+        }
+    }
+    // 如果找不到目標 cookie，返回空值
+    return "";
+}
+
+// 顯示 flag 的函數
+function showFlag() {
+    // 在 message-box 中顯示 flag
+    var messageBox = document.querySelector('.message-box');
+    messageBox.innerHTML += '<p>Flag: Flag is here!</p>';
+}
+
+// 檢查 login cookie 的值
+var loginValue = getCookieValue("login");
+
+// 如果 login cookie 的值為 1，顯示 flag
+if (loginValue === "1") {
+    showFlag();
+}
+
+// 設置 login cookie 的函數
+function setLoginCookie(value) {
+    // 設置 login cookie 的值
+    document.cookie = "login=" + value;
+}
+
+// 設置 login cookie 的值為 1
+setLoginCookie("1");
