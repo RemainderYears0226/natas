8e7ae6cc5c38029e5bfd337dbd82eab9ba0f881e
RemainderYears0226 <144605230+RemainderYears0226@users.noreply.github.com>
2024-04-24 11:44:10 +0800

    Update script.js

--- level 2/(index)/script.js
+++ level 2/(index)/script.js
@@ -1,36 +1,27 @@
 // 第二關的flag-3/3: Y29kZX0=
 document.addEventListener('contextmenu', function(e) {
-    alert('在這裡右鍵不能使用喔!');
-    e.preventDefault();
+  alert('在這裡右鍵不能使用喔!');
+  e.preventDefault();
 }, false);
 
 function getCookieValue(cookieName) {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i].trim();
-        var cookieParts = cookie.split('=');
-        if (cookieParts[0] === cookieName) {
-            return cookieParts[1]; // 返回 cookie 的值
-        }
+  var cookies = document.cookie.split(';');
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length + 1);
     }
-    return "";
+  }
+  return "";
 }
 
-
 function showFlag() {
-    var messageBox = document.querySelector('.message-box');
-    messageBox.innerHTML += '<p>Flag: Flag is here!</p>';
+  var messageBox = document.querySelector('.message-box');
+  messageBox.innerHTML += '<p>恭喜你找到了 Flag! 請透過 F12 開發者工具中的 Application 標籤,將 login Cookie 值改為 1 來顯示 Flag。</p>';
+  messageBox.innerHTML += '<p>Flag: Flag is here!</p>';
 }
 
 var loginValue = getCookieValue("login");
-
 if (loginValue === "1") {
-    showFlag();
+  showFlag();
 }
-
-function setLoginCookie(value) {
-    // 設置 login cookie 的值
-    document.cookie = "login=" + value;
-}
-
-setLoginCookie("0");
