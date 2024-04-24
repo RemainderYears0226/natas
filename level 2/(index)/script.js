ffa4720c95bbee7064fed6a5e0b15764b0b0a8a5
RemainderYears0226 <144605230+RemainderYears0226@users.noreply.github.com>
2024-04-24 12:05:49 +0800

    Update script.js

--- level 2/(index)/script.js
+++ level 2/(index)/script.js
@@ -3,31 +3,36 @@ document.addEventListener('contextmenu', function(e) {
     alert('在這裡右鍵不能使用喔!');
     e.preventDefault();
 }, false);
-function getCookieValue(cookieName) {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i].trim();
-        if (cookie.indexOf(cookieName) === 0) {
-            return cookie.substring(cookieName.length + 1);
+
+document.addEventListener('DOMContentLoaded', function() {
+    var loginValue = getCookieValue("login");
+
+    if (loginValue === "1") {
+        showFlag();
+    }
+
+    function showFlag() {
+        var messageBox = document.querySelector('.message-box');
+        if (messageBox) {
+            messageBox.innerHTML += '<p>Flag: Flag is here!</p>';
         }
     }
-    return "";
-}
 
-function showFlag() {
-    var messageBox = document.querySelector('.message-box');
-    messageBox.innerHTML += '<p>Flag: Flag is here!</p>';
-}
+    function getCookieValue(cookieName) {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.indexOf(cookieName) === 0) {
+                return cookie.substring(cookieName.length + 1);
+            }
+        }
+        return "";
+    }
 
-var loginValue = getCookieValue("login");
+    function setLoginCookie(value) {
+        // 設置 login cookie 的值
+        document.cookie = "login=" + value;
+    }
 
-if (loginValue === "1") {
-    showFlag();
-}
-
-function setLoginCookie(value) {
-    // 設置 login cookie 的值
-    document.cookie = "login=" + value;
-}
-
-setLoginCookie("0");
+    setLoginCookie("0");
+});
