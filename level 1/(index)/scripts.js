88fc192f30d14d3c3f38030c78e76397a9fd8bf9
RemainderYears0226 <144605230+RemainderYears0226@users.noreply.github.com>
2024-04-26 16:22:01 +0800

    Delete level 1/(index)/scripts.js

--- level 1/(index)/scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var userInput = document.getElementById('user-input');
-
-    userInput.addEventListener('input', function() {
-        document.querySelector('.message-box').innerHTML = userInput.value;
-    });
-});
