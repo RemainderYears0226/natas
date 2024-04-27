628e76faa908131471f5d9a5841e9f0cfd62ef59
RemainderYears0226 <144605230+RemainderYears0226@users.noreply.github.com>
2024-04-28 01:28:59 +0800

    Update scripts.js

--- level 3/(index)/scripts.js
+++ level 3/(index)/scripts.js
@@ -1,12 +1,1 @@
-const googleLoginButton = document.querySelector('.google-login-button');
 
-googleLoginButton.addEventListener('click', () => {
-    // Simulate Google login process (for demonstration purposes)
-    const userLoggedIn = confirm('模擬 Google 登入？');
-
-    if (userLoggedIn) {
-        alert('這是錯誤的地方。');
-    } else {
-        alert('你找到了正確的地方！');
-    }
-});
