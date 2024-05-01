287f0b14f0cb56a23ee3eb82b9c5f1b9fae0504c
RemainderYears0226 <144605230+RemainderYears0226@users.noreply.github.com>
2024-05-02 01:40:28 +0800

    Update script.js

--- 新增資料夾/script.js
+++ 新增資料夾/script.js
@@ -53,4 +53,70 @@ searchPageButton.addEventListener('click', () => {
 // Redirect to search page
 window.location.href = 'search.html';
 });
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const searchResults = document.getElementById('searchResults');
 
+// 假設的臨時資料庫
+const tempDatabase = [
+  '這是第一筆資料',
+  '這是包含關鍵字的第二筆資料',
+  '第三筆資料也包含了關鍵字',
+  '這是最後一筆資料'
+];
+
+searchButton.addEventListener('click', () => {
+  const searchTerm = searchInput.value.trim().toLowerCase();
+  if (searchTerm) {
+    searchResults.innerHTML = '';
+    const matchedResults = tempDatabase.filter(result =>
+      result.toLowerCase().includes(searchTerm)
+    );
+    if (matchedResults.length > 0) {
+      matchedResults.forEach(result => {
+        const resultElement = document.createElement('p');
+        resultElement.textContent = result;
+        searchResults.appendChild(resultElement);
+      });
+    } else {
+      searchResults.innerHTML = '<p>沒有符合的搜尋結果</p>';
+    }
+  } else {
+    searchResults.innerHTML = '<p>請輸入關鍵字</p>';
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+var loginValue = getCookieValue("admin");
+if (loginValue === "True") {
+showFlag();
+}
+function showFlag() {
+var messageBox = document.createElement('div');
+messageBox.classList.add('message-box');
+messageBox.innerHTML = '<p>Flag: FHSH{c00kie=1}</p>';
+messageBox.style.position = 'fixed';
+messageBox.style.top = '50%';
+messageBox.style.left = '50%';
+messageBox.style.transform = 'translate(-50%, -50%)';
+messageBox.style.backgroundColor = '#fff';
+messageBox.style.padding = '20px';
+messageBox.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+messageBox.style.zIndex = '9999';
+document.body.appendChild(messageBox);
+}
+function getCookieValue(cookieName) {
+var cookies = document.cookie.split(';');
+for (var i = 0; i < cookies.length; i++) {
+var cookie = cookies[i].trim();
+if (cookie.indexOf(cookieName) === 0) {
+return cookie.substring(cookieName.length + 1);
+}
+}
+return "";
+}
+function setLoginCookie(value) {
+document.cookie = "admin=" + value;
+}
+setLoginCookie("False");
+});
